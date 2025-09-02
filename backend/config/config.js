753906fb8d48fd7763f84cbf738ea4739de4ec03
Krish Patel <krishpatel6666@gmail.com>
2025-09-01 21:36:37 -0400

    Reinitialized mono-repo structure

--- backend/config/config.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export default {
-  port: process.env.PORT,
-  twitchClientId: process.env.TWITCH_CLIENT_ID,
-  twitchAccessToken: process.env.TWITCH_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI
-};
